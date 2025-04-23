fe9c2c16a0cc3234bd706ae2a3bd52bedc1b85e7
UriSmu <48240113@ort>
2025-04-23 10:05:36 -0300

    Agregar UseState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
+import { useState } from 'react';
 import Formulario from './Formulario';
 import Cita from './Cita';
 import Listado from './Listado';
 
 function App() {
+  const [citas, setCitas] = useState([
+    {
+      id: 1,
+      mascota: "Shaina",
+      propietario: "Uriel Smucler",
+      fecha: "29/04",
+      hora: "10:20",
+      sintomas: "Control rutinario"
+    },
+    {
+      id: 2,
+      mascota: "Pluto",
+      propietario: "Mickey Mouse",
+      fecha: "26/04",
+      hora: "15:40",
+      sintomas: "Resfrío"
+    },
+    {
+      id: 3,
+      mascota: "Negrita",
+      propietario: "Pedro Gómez",
+      fecha: "22/04",
+      hora: "17:00",
+      sintomas: "Baño y corte higienico"
+    }
+  ]);
 
   return (
     <>
       <h1>Administrador de Pacientes</h1>
-      <div class="container">
-        <Formulario/>
-        <Listado/>
+      <div className="container">
+        <Formulario />
+        <Listado citas={citas} />
       </div>
     </>
   );
--- src/Listado/index.jsx
+++ src/Listado/index.jsx
@@ -1,17 +1,22 @@
 import './Listado.css';
 import Cita from '../Cita';
-let listaCitas = (
-  <div class="one-half column">
-    <h2>Administra tus citas</h2>
-    <Cita mascota="Shaina" propietario="Uriel Smucler" fecha="29/04" hora="10:20" sintomas="Control rutinario"/>
-    <Cita mascota="Pluto" propietario="Mickey Mouse" fecha="26/04" hora="15:40" sintomas="Resfrío"/>
-    <Cita mascota="Negrita" propietario="Pedro Gómez" fecha="22/04" hora="17:00" sintomas="Baño y corte higienico"/>
-  </div>
-)
-const Listado = () => {
+
+const Listado = ({ citas }) => {
   return (
-    listaCitas
-  )
+    <div className="one-half column">
+      <h2>Administra tus citas</h2>
+      {citas.map((cita) => (
+        <Cita
+          key={cita.id}
+          mascota={cita.mascota}
+          propietario={cita.propietario}
+          fecha={cita.fecha}
+          hora={cita.hora}
+          sintomas={cita.sintomas}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Listado;
